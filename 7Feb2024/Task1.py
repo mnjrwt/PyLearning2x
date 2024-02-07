dd5e08ab5467bc2c57a9b71c5deae706b3bfd851
mnjrwt <rawat.manoj786@gmail.com>
2024-02-07 17:19:05 +0530

    Task1 done

---
+++ 7Feb2024/Task1.py
@@ -0,0 +1,33 @@
+# Assignment operator '=' is used to assign value into identifier
+name = "Manoj"
+
+# Equals to Operator
+Result = (name == "Shiva")
+
+print("Assigned Value:", name)
+print(type(name))
+
+print("Is name correct?", Result)
+print(type(Result))
+
+# Operator Exponentiation (**) treats as Power
+num1 = 5
+num2 = 3
+output = (num1 ** num2)
+print(output)
+
+# Bitwise Operator- XOR (^)
+# Decimal numbers and their binary values:
+# 0 = 0000000000000000
+# 1 = 0000000000000001
+# 2 = 0000000000000010
+# 3 = 0000000000000011
+# 4 = 0000000000000100
+# 5 = 0000000000000101
+# 6 = 0000000000000110
+# 7 = 0000000000000111
+
+n1 = 8
+n2 = 6
+value = n1 ^ n2
+print(value)
