1478cb1b18200c908ad379e9e7e7618442666799
mnjrwt <rawat.manoj786@gmail.com>
2024-02-07 17:32:02 +0530

    Program for Area of Circle

---
+++ 7Feb2024/area_circle.py
@@ -0,0 +1,14 @@
+# area of a circle
+
+radius = input("Enter the radius of Circle:")
+
+# Convert the str into int
+r = int(radius)
+
+pie = 3.14
+
+area = pie * (r ** 2)
+print("Area of Circle is =", area)
+
+
+
